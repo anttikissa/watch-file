62afd8fab299b09612ae9ad719fac48bb266d4cf
Antti Sykäri <antti.sykari@gmail.com>
2012-12-12 12:31:13 +0200

    Basic functionality.

--- watch-file.js
+++ watch-file.js
@@ -1,2 +1,11 @@
-console.log("Hi!");
+var fs = require('fs');
+
+var args = process.argv.splice(2)
+
+args.forEach(function(file) {
+	console.log(file);
+	fs.watch(file, function(event, filename) {
+		process.exit(0);
+	});
+});
 
